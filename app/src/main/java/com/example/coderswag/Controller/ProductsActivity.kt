17b23bbb1b9f517c4990cd511b717be1ab1cca7b
hyungilee <mike.hyungi.lee@gmail.com>
2020-01-19 09:57:34 +0900

    lesson 08

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -1,11 +1,18 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.coderswag.Adapter.ProductAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +20,22 @@ class ProductsActivity : AppCompatActivity() {
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
         println(categoryType)
+        adapter = ProductAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        // check orientation return integer number(1: portrait 2: landscape)
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -24,17 +24,30 @@ object DataService {
         Product("Devslopes Graphic Beanie","$18","hat1"),
         Product("Devslopes Graphic Black","$20","hat2"),
         Product("Devslopes Graphic White","$18","hat3"),
+        Product("Devslopes Graphic Snapback","$22","hat4"),
+        Product("Devslopes Graphic Beanie","$18","hat1"),
+        Product("Devslopes Graphic Black","$20","hat2"),
+        Product("Devslopes Graphic White","$18","hat3"),
         Product("Devslopes Graphic Snapback","$22","hat4")
     )
 
     val hoodies = listOf(
         Product("Devslopes Hoodie Gray","$28", "hoodie1"),
         Product("Devslopes Hoodie Red","$30", "hoodie2"),
-        Product("Devslopes Hoodie Blue","$40", "Hoodie3"),
-        Product("Devslopes Hoodie Brown","$35", "Hoodie4")
+        Product("Devslopes Hoodie Blue","$40", "hoodie3"),
+        Product("Devslopes Hoodie Brown","$35", "hoodie4"),
+        Product("Devslopes Hoodie Gray","$28", "hoodie1"),
+        Product("Devslopes Hoodie Red","$30", "hoodie2"),
+        Product("Devslopes Hoodie Blue","$40", "hoodie3"),
+        Product("Devslopes Hoodie Brown","$35", "hoodie4")
     )
 
     val shirts = listOf(
+        Product("Devslopes Shirt black","$28", "shirt1"),
+        Product("Devslopes Shirt Red","$20", "shirt2"),
+        Product("Devslopes Shirt Blue","$30", "shirt3"),
+        Product("Devslopes Shirt Brown","$35", "shirt4"),
+        Product("Kickflip Studios","$18", "shirt5"),
         Product("Devslopes Shirt black","$28", "shirt1"),
         Product("Devslopes Shirt Red","$20", "shirt2"),
         Product("Devslopes Shirt Blue","$30", "shirt3"),
